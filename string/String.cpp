7d0d4738711a3040e7d0db05401c5814efdc617c
AlexeyPazyura <alexeypazyura@gmail.com>
2024-01-09 19:27:38 +0200

    Some fixes for the String class

--- string/String.cpp
+++ string/String.cpp
@@ -1,6 +1,7 @@
 #include "String.h"
 using namespace tc;
 
+// CONSTRUCTORS
 String::String(const char *string)
 {
     m_size = strlen(string);
@@ -17,7 +18,10 @@ String::~String()
 {
     delete[] m_string;
 }
-const String& String::operator=(String &string)
+
+
+// OPERATOR OVERLOADINGS
+String& String::operator=(const String &string)
 {
     if (this == &string)
         return *this;
@@ -25,20 +29,20 @@ const String& String::operator=(String &string)
     m_size = string.m_size;
     delete[] m_string;
     m_string = new char[m_size + 1];
-    std::strcpy(m_string, string.m_string);
+    strcpy(m_string, string.m_string);
 
     return *this;
 }
-const String& String::operator=(const char *string)
+String& String::operator=(const char *string)
 {
     m_size = strlen(string);
     delete[] m_string;
     m_string = new char[m_size + 1];
-    std::strcpy(m_string, string);
+    strcpy(m_string, string);
 
     return *this;
 }
-const String& String::operator+=(String &string)
+const String& String::operator+=(const String &string)
 {
     m_size += string.m_size;
     char *new_string = new char[m_size + 1];
@@ -60,26 +64,26 @@ const String& String::operator+=(const char *string)
 
     return *this;
 }
-const String& String::operator+(String &string) const
+const String String::operator+(const String &string) const
 {
     char *new_string = new char[m_size + string.m_size + 1];
     strcpy(new_string, m_string);
     strcat(new_string, string.m_string);
-    return *(new String(new_string));
+    return String(new_string);
 }
-const String& String::operator+(const char *string) const
+const String String::operator+(const char *string) const
 {
     char *new_string = new char[m_size + strlen(string) + 1];
     strcpy(new_string, m_string);
     strcat(new_string, string);
-    return *(new String(new_string));
+    return String(new_string);
 }
-const String& tc::operator+(const char *literal, String &string)
+const String tc::operator+(const char *literal, const String &string)
 {
     char *new_string = new char[string.m_size + strlen(literal) + 1];
     strcpy(new_string, literal);
     strcat(new_string, string.m_string);
-    return *(new String(new_string));
+    return String(new_string);
 }
 std::ostream& tc::operator<<(std::ostream &out, const String &string)
 {
--- string/String.h
+++ string/String.h
@@ -20,15 +20,19 @@ namespace tc
         inline std::size_t size() const
         { return m_size; }
 
-        const String& operator+=(String &string);
-        const String& operator+=(const char *string);
-        const String& operator+(String &string) const;
-        const String& operator+(const char *string) const;
-        friend const String& operator+(const char *literal, String &string);
+        inline char operator[](size_t index) const
+        { return m_string[index]; }
 
-        const String& operator=(String &string);
-        const String& operator=(const char *string);
+        const String& operator+=(const String &string);
+        const String& operator+=(const char *string);
+        const String operator+(const String &string) const;
+        const String operator+(const char *string) const;
+        friend const String operator+(const char *literal, const String &string);
+
+        String& operator=(const String &string);
+        String& operator=(const char *string);
         friend std::ostream& operator<<(std::ostream &out, const String &string);
+        friend std::istream& operator>>(std::istream &in, const String &string);
     };
 }
 
